cb7b5fbb5fba83900aa9614dd88c1871295d17b2
Aayush oli <aayusholi57@gmail.com>
2026-09-18 01:09:29 +0545

    Fix age formatting in day 1 exercise

--- day 1_class work/file_4.py
+++ day 1_class work/file_4.py
@@ -1,5 +1,6 @@
-name= "aayush"
-age= 21
-coding="2 years of coding"
-print(name,age,coding)
-print(name+","+ "age")
+name = "aayush"
+age = 21
+coding = "2 years of coding"
+
+print(name, age, coding)
+print(name + "," + str(age))
